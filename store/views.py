f4c93ee756c58f5ce8e7eac6f3a333f78cd00e72
Stephen Smith <stephenlehanesmith@gmail.com>
2023-04-08 15:22:32 +0100

    displayed products by category

--- store/views.py
+++ store/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from store.models import Product
+from category.models import Category
 
-def store(request):
-    products = Product.objects.all().filter(is_available=True)
+def store(request, category_slug=None):
+    category = None
+    products = None
+    if category_slug:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
+
     context = {
         'products': products,
-        'product_count': products.count()
+        'product_count': product_count
     }
     return render(request, 'store/store.html', context)

cc841c309677e6f939f9a995a5916aacae1e5801
Stephen Smith <stephenlehanesmith@gmail.com>
2023-04-08 15:15:22 +0100

    displayed products in store page

--- store/views.py
+++ store/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from store.models import Product
 
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.all().filter(is_available=True)
+    context = {
+        'products': products,
+        'product_count': products.count()
+    }
+    return render(request, 'store/store.html', context)

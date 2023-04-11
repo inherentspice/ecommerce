977e8ba689b717ea030b3bc7eb6ca87c7bd63585
Stephen Smith <stephenlehanesmith@gmail.com>
2023-04-11 12:14:44 +0100

    got the instance of the variation within the view

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 
 def _get_card_id(request):
@@ -9,9 +9,19 @@ def _get_card_id(request):
     return cart
 
 def add_cart(request, product_id):
-    color = request.GET['color']
-    size = request.GET['size']
     product = Product.objects.get(id=product_id)
+    product_variation = []
+    if request.method == "POST":
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
+
     try:
         cart = Cart.objects.get(cart_id=_get_card_id(request))
     except:

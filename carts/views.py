635487bbf3d216512cbe98eeb057e5f6b01c24a2
Stephen Smith <stephenlehanesmith@gmail.com>
2023-04-11 19:56:30 +0100

    grouped items by variation in the cart

--- carts/views.py
+++ carts/views.py
@@ -28,20 +28,34 @@ def add_cart(request, product_id):
         cart = Cart.objects.create(cart_id=_get_card_id(request))
     cart.save()
 
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
+    is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
+
+    if is_cart_item_exists:
+        cart_item = CartItem.objects.filter(product=product, cart=cart)
+        existing_variation_list = []
+        id = []
+        for item in cart_item:
+            existing_variation = item.variations.all()
+            existing_variation_list.append(list(existing_variation))
+            id.append(item.id)
+
+        if product_variation in existing_variation_list:
+            index = existing_variation_list.index(product_variation)
+            item_id = id[index]
+            item = CartItem.objects.get(product=product, id=item_id)
+            item.quantity += 1
+            item.save()
+        else:
+            item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+            if len(product_variation) > 0:
+                item.variations.clear()
+                item.variations.add(*product_variation)
+            item.save()
+    else:
+        cart_item = CartItem.objects.create(product=product, cart=cart, quantity=1)
         if len(product_variation) > 0:
             cart_item.variations.clear()
-            for item in product_variation:
-                cart_item.variations.add(item)
-        cart_item.quantity += 1
-        cart_item.save()
-    except:
-        cart_item = CartItem.objects.create(product=product, cart=cart, quantity=1)
-        cart_item.variations.clear()
-        if len(product_variation) > 0:
-            for item in product_variation:
-                cart_item.variations.add(item)
+            cart_item.variations.add(*product_variation)
         cart_item.save()
 
     return redirect('cart')

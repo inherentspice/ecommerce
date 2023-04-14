0304bfe3e1c050be4a0d8976d2308bcae9c4ca33
Stephen Smith <stephenlehanesmith@gmail.com>
2023-04-14 10:32:44 +0100

    set up reset password validation

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('', views.dashboard, name='dashboard'),
     path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
+    path('resetPassword/', views.resetPassword, name='resetPassword'),
     path('reset_password_validate/<uidb64>/<token>', views.reset_password_validate, name='reset_password_validate'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]

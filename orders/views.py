8bf8425216d20f9e235f2de5d23b8a6e8b589797
Stephen Smith <stephenlehanesmith@gmail.com>
2023-04-22 15:29:24 +0100

    added dynamic values to payment page

--- orders/views.py
+++ orders/views.py
@@ -6,6 +6,7 @@ import datetime
 
 def payments(request):
     return render(request, 'orders/payments.html')
+
 def place_order(request, total=0, quantity=0):
     current_user = request.user
 
@@ -24,7 +25,7 @@ def place_order(request, total=0, quantity=0):
         form = OrderForm(request.POST)
         if form.is_valid():
             data = Order()
-            data.user - current_user
+            data.user = current_user
             data.first_name = form.cleaned_data['first_name']
             data.last_name = form.cleaned_data['last_name']
             data.phone = form.cleaned_data['phone']
@@ -48,6 +49,15 @@ def place_order(request, total=0, quantity=0):
             order_number = current_date + str(data.id)
             data.order_number = order_number
             data.save()
-            return redirect('checkout')
+
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total
+            }
+            return render(request, 'orders/payments.html', context)
     else:
         return redirect('checkout')
